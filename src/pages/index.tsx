f65d55cb285116ae05a7170d888f9c711b793979
swapnilmittal <swapnil.mittal1@gmail.com>
2025-09-21 22:12:08 -0400

    revert(hero): restore original Spline robot animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,11 +43,6 @@ const SimpleSpline = dynamic(() => import("@/components/SimpleSpline"), {
   ssr: false,
 });
 
-// Load Interactive Avatar
-const InteractiveAvatar = dynamic(() => import("@/components/InteractiveAvatar"), {
-  ssr: false,
-});
-
 const projects = [
   {
     title: "Reverse-Proxy API Debugger",
@@ -369,7 +364,10 @@ export default function Home() {
             id={styles["canvas-container"]}
             className="mt-full h-96 w-96 xl:mt-0"
           >
-            <InteractiveAvatar className="h-full w-full" />
+            <SimpleSpline
+              scene="/assets/robot_follow_cursor_for_landing_page.spline"
+              className="h-full w-full"
+            />
           </div>
 </section>
 
